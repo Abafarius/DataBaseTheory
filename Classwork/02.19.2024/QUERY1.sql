58564f73c493dfdd9006bd27126e1de696465ca3
Yessimbek Galymzhan <esm.2001@mail.ru>
2024-02-19 21:48:26 +0600

    Create QUERY1.sql

---
+++ Classwork/02.19.2024/QUERY1.sql
@@ -0,0 +1,70 @@
+CREATE TABLE rpo232(
+	id INT IDENTITY,
+	fio NVARCHAR(120) NOT NULL,
+	[avg] FLOAT DEFAULT 12,
+	count_stars INT NOT NULL,
+	fav_subj nvarchar(150) NOT NULL
+)
+
+
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Renault Domenget', 11, 553.19, 'Web Developer IV');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Avril Keatch', 6, 975.22, 'Senior Editor');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Abagael Adam', 1, 839.87, 'Senior Editor');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Pen Titterton', 6, 793.71, 'Automation Specialist III');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Willyt Elderidge', 4, 431.14, 'Health Coach IV');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Frederick Holbie', 11, 404.21, 'Payment Adjustment Coordinator');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Kathi Kreuzer', 4, 563.88, 'Operator');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Neddie Maciunas', 1, 793.3, 'Database Administrator I');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Peter Gerold', 6, 362.85, 'Account Coordinator');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Florina Midden', 6, 484.12, 'VP Product Management');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Yvon Barthrop', 7, 629.18, 'Compensation Analyst');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Svend Bunner', 3, 633.75, 'VP Sales');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Amandy Grishukhin', 9, 336.31, 'Programmer I');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Claudetta Ivchenko', 2, 44.9, 'Senior Sales Associate');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Margalo Edmonston', 6, 329.25, 'Engineer II');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Penrod Trustram', 8, 309.82, 'Design Engineer');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Franny Borris', 3, 662.75, 'Dental Hygienist');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Margaretha Goodreid', 0, 561.83, 'Quality Engineer');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Rickie Ranyelld', 7, 871.12, 'Actuary');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Paola Ozanne', 2, 720.35, 'Legal Assistant');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Shayne Jozwik', 0, 122.19, 'Legal Assistant');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Devon Bishell', 6, 284.66, 'Electrical Engineer');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Cher Ciciotti', 2, 928.64, 'Help Desk Technician');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Skye Loosemore', 7, 857.93, 'Environmental Specialist');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Costa Stilly', 12, 825.35, 'Environmental Specialist');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Malchy Harses', 1, 466.55, 'Engineer II');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Yasmeen Sidey', 9, 265.61, 'Account Representative IV');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Dugald Throssell', 7, 703.56, 'Staff Scientist');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Gwenore Hamly', 1, 763.45, 'VP Quality Control');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Cris Melloy', 9, 205.59, 'Safety Technician III');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Felic Kurt', 8, 173.57, 'Web Developer II');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Lesley Ryding', 8, 623.18, 'Associate Professor');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Richie Banbridge', 1, 578.18, 'Senior Quality Engineer');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Fairleigh Crop', 3, 455.58, 'Media Manager III');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Anne Tierney', 0, 809.44, 'Software Consultant');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Sharon Rudyard', 6, 111.49, 'Chief Design Engineer');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Tiebout Moxon', 11, 601.17, 'Dental Hygienist');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Dorey Briffett', 12, 395.85, 'VP Quality Control');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Boyd Dickings', 0, 927.24, 'Research Assistant IV');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Austin Simic', 3, 14.88, 'Teacher');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Kerk Lockier', 11, 988.47, 'Internal Auditor');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Myrtle Brade', 10, 44.76, 'Staff Accountant I');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Vail Avon', 12, 12.11, 'Safety Technician III');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Cynthia Bert', 2, 668.02, 'Administrative Assistant III');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Pierre Cabrales', 1, 992.83, 'Chief Design Engineer');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Patrica Spurway', 7, 255.44, 'VP Accounting');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Carlotta Sterricker', 2, 408.74, 'Recruiting Manager');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Lelia Kincade', 2, 378.95, 'Systems Administrator I');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Dorree Pettendrich', 10, 292.76, 'Legal Assistant');
+insert into rpo232 (fio, avg, count_stars, fav_subj) values ('Angel Dannohl', 10, 194.46, 'Design Engineer');
+
+
+UPDATE rpo232 
+SET fio = 'Alan'
+WHERE id = 12
+
+DELETE FROM rpo232
+WHERE id = 12
+
+SELECT *
+FROM rpo232
